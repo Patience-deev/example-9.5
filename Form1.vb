726dd8923c182a6da96cc56753a9a895e9567cee
Dell-User <Dell-User@DESKTOP-78SJPVI>
2023-10-04 20:16:34 +0200

    Add project files.

---
+++ Form1.vb
@@ -0,0 +1,11 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim myPhrase As String
+        Dim LblPhrase
+        Dim blExtract
+
+        myPhrase = InputBox("enter your phrase")
+        LblPhrase = myPhrase
+        blExtract = MsgBox(Mid(myPhrase, 2, 6))
+    End Sub
+End Class
